210e29ac86ec6f1b31c157ae76628cc4183f8eb6
Daniel Aleksandersen <code@daniel.priv.no>
2018-08-28 06:05:58 +0200

    Better titles and URLs
    * Parse Open Graph and other metadata for better titles and canonical links.
    * Default to sharing to HTTPS.

--- mast-share.js
+++ mast-share.js
@@ -7,9 +7,14 @@ document.addEventListener("DOMContentLoaded", function() {
 		q.querySelector('.mast-share-button').addEventListener('click', function(e) {
 			var webreg=new RegExp("^(?:(?:https?|ftp)://)?(?:\\S+(?::\\S*)?@|\\d{1,3}(?:\\.\\d{1,3}){3}|(?:(?:[a-z\\d\\u00a1-\\uffff]+-?)*[a-z\\d\\u00a1-\\uffff]+)(?:\\.(?:[a-z\\d\\u00a1-\\uffff]+-?)*[a-z\\d\\u00a1-\\uffff]+)*(?:\\.[a-z\\u00a1-\\uffff]{2,6}))(?::\\d+)?(?:[^\\s]*)?$","i");
 			var options = "toolbar=no,location=no,status=yes,resizable=yes,scrollbars=yes,height=600,width=400";
-			var instance = q.querySelector('input[name="mast-instance-input"]')
+			var instance = q.querySelector('input[name="mast-instance-input"]');
+			var url_link = document.querySelector('link[rel~="canonical"][href],link[rel~="og:url"][href]'),
+			    url_meta = document.querySelector('meta[property~="og:url"][content]'),
+			    doc_url = url_link ? url_link.href : (url_meta ? url_meta.content : document.location.href);
+			var title = document.querySelector('meta[property~="og:title"][content],meta[property~="dc:title"][content],meta[name="title"][content]');
+			title = title ? title.content : document.title;
 			if(webreg.test(instance.value)) {
-				var url = `http:\/\/${instance.value.replace(/(^\w+:|^)\/\//, '')}/share?text=${encodeURIComponent(document.title)} ${encodeURIComponent(location.href)}`;
+				var url = `https:\/\/${instance.value.replace(/(^\w+:|^)\/\//, '')}/share?text=${encodeURIComponent(title)} ${encodeURIComponent(doc_url)}`;
 				window.open(url, "new", options);
 			} else {
 				instance.classList.add('invalid')
